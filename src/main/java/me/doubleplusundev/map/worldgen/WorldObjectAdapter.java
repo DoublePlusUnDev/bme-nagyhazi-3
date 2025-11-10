d1a380a6ae23dffcbcb01e8e18a1ea6e4ea22021
DoublePlusUnDev <andri.takacs@gmail.com>
2025-11-10 13:58:02 +0100

    Google Json

---
+++ src/main/java/me/doubleplusundev/map/worldgen/WorldObjectAdapter.java
@@ -0,0 +1,45 @@
+package me.doubleplusundev.map.worldgen;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+import me.doubleplusundev.map.WorldObject;
+import me.doubleplusundev.map.structures.Center;
+import me.doubleplusundev.map.structures.Road;
+
+public class WorldObjectAdapter implements JsonSerializer<WorldObject>, JsonDeserializer<Object> {
+
+    @Override
+    public JsonElement serialize(WorldObject src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject obj = context.serialize(src).getAsJsonObject();
+        if (src instanceof Center) obj.addProperty("wotype", "Center");
+        if (src instanceof Road) obj.addProperty("wotype", "Road");
+        return obj;
+    }
+
+    @Override
+    public Object deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject obj = json.getAsJsonObject();
+        String type = obj.get("wotype").getAsString();
+        switch (type) {
+            case "Center" -> {
+                return context.deserialize(json, Center.class);
+            }
+            case "Road" -> {
+                return context.deserialize(json, Road.class);
+            }
+            default -> throw new RuntimeException("Unknown type:" + type);
+        }
+
+    }
+    
+    
+    
+}
--- src/main/java/me/doubleplusundev/savegame/SaveGameManager.java
+++ src/main/java/me/doubleplusundev/savegame/SaveGameManager.java
@@ -1,27 +1,35 @@
 package me.doubleplusundev.savegame;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 
 import me.doubleplusundev.map.GameMap;
 import me.doubleplusundev.map.GameMapHandler;
+import me.doubleplusundev.map.WorldObject;
+import me.doubleplusundev.map.worldgen.WorldObjectAdapter;
 
 public class SaveGameManager {
+    Gson gson;
     private final GameMapHandler gameMapHandler;
 
     private String saveGameLocation;
 
     public SaveGameManager(GameMapHandler gameMapHandler) {
         this.gameMapHandler = gameMapHandler;
+        this.gson = new GsonBuilder().registerTypeAdapter(WorldObject.class, new WorldObjectAdapter()).create();
     }
 
 
     public void save() {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(saveGameLocation))){
-            oos.writeObject(gameMapHandler.getMap());
+        String jsonString = gson.toJson(gameMapHandler.getMap());
+        try (FileWriter fw = new FileWriter(saveGameLocation)){
+            fw.write(jsonString);
         }
         catch (IOException exception) {
             exception.printStackTrace();
@@ -29,10 +37,11 @@ public class SaveGameManager {
     }
 
     public void load() {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(saveGameLocation))) {
-            gameMapHandler.setMap((GameMap)ois.readObject());
+        try {
+            String jsonString = Files.readString(new File(saveGameLocation).toPath());
+            gameMapHandler.setMap(gson.fromJson(jsonString, GameMap.class));
         }
-        catch (IOException | ClassNotFoundException exception) {
+        catch (IOException | JsonSyntaxException exception) {
             exception.printStackTrace();
         }
         
